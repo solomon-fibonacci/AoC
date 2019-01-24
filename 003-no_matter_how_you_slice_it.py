2160c3053cde15d1e0eb9686d945000ee6a1ca8e
Solomon Omojola <omojolasolomon@gmail.com>
2019-01-24 23:34:26 +0100

    Tweaking tweaking.. day 3 still

--- 003-no_matter_how_you_slice_it.py
+++ 003-no_matter_how_you_slice_it.py
@@ -6,7 +6,7 @@ class FabricCutter:
     def get_overlaps(self):
         with open(self.input) as fabric:
             overlap_counter = 0
-            all_square_inch_coordinates = []
+            all_square_inch_coordinates = {}
             for fabric_cut in fabric:
                 print(fabric_cut)
                 describer_array = fabric_cut.split(' ')
@@ -15,15 +15,28 @@ class FabricCutter:
                 describer_array[3] = describer_array[3].split('x')
                 describer_array[3][1] = describer_array[3][1][:-1]
                 print(describer_array)
-                for col in range(int(describer_array[2][0]),
-                                 int(describer_array[2][0] + describer_array[3][0])):
-                    for row in range(int(describer_array[2][1]),
-                                     int(describer_array[2][1]) + int(describer_array[3][1])):
+                first_x, first_y = int(describer_array[2][0]) + 1, int(describer_array[2][1]) + 1
+                width = int(describer_array[3][0])
+                height = int(describer_array[3][1])
+
+                for col in range(int(describer_array[2][0]) + 1,
+                                 int(describer_array[2][0]) + 1 + int(describer_array[3][0])):
+                    for row in range(int(describer_array[2][1]) + 1,
+                                     int(describer_array[2][1]) + 1 + int(describer_array[3][1])):
+                        # import pdb; pdb.set_trace()
                         sq_inch_coord = (col, row)
-                        print(sq_inch_coord)
-                        if sq_inch_coord in all_square_inch_coordinates:
-                            overlap_counter += 1
-                        all_square_inch_coordinates.append(sq_inch_coord)
+                        print('{}{}'.format(describer_array[0], sq_inch_coord))
+                        try:
+                            all_square_inch_coordinates[str(sq_inch_coord)] += 1
+                        except KeyError:
+                            all_square_inch_coordinates[str(sq_inch_coord)] = 1
+
+
+            for coord, count in all_square_inch_coordinates.items():
+                print('{}-->{}'.format(coord, count))
+                if count > 1:
+                    overlap_counter += 1
+                        
         return overlap_counter
 
 
